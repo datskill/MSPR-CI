e181f339321b66ee6739778ca4716a1becbb1c32
hugo thomas <thomas-hugo@orange.fr>
2020-03-30 16:54:40 +0200

    Refacto repo

--- src/main/java/com/preudhomme/erp/back/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.preudhomme.erp.back.repository;
-
-import com.preudhomme.erp.back.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
